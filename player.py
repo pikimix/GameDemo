6bf6bf7979ef4ef30df207a75547e21d7cdb4b45
Keith Summers <pikimix@gmail.com>
2024-08-06 21:26:15 +0100

    update player to use sprite sheet, create sprite sheet

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
+from sprite_sheet import Sprite
 class Player:
     def __init__(self, location, sprite) -> None:
         self.location = location
-        self.sprite = sprite
+        self.sprite = Sprite(sprite)
+        self.facing_left = False
 
     def update(self, pygame, dt) -> None:
         keys = pygame.key.get_pressed()
@@ -10,9 +12,12 @@ class Player:
         if keys[pygame.K_s]:
             self.location.y += 300 * dt
         if keys[pygame.K_a]:
+            self.facing_left = True
             self.location.x -= 300 * dt
         if keys[pygame.K_d]:
+            self.facing_left = False
             self.location.x += 300 * dt
+        self.sprite.update()
 
     def draw(self, screen):
-        screen.blit(self.sprite, self.location, None)
+        self.sprite.draw(screen, self.location, flip=self.facing_left)
---
+++ sprite_sheet.py
@@ -0,0 +1,20 @@
+from pygame import Rect, time, transform
+
+class Sprite:
+    def __init__(self, image) -> None:
+        self.image = image
+        self.frame = Rect(0,0,16,32)
+        self.frame_update = 1000/10
+        self.last_frame = 0
+
+    def update(self):
+        current_time = time.get_ticks()
+        if current_time > self.last_frame + self.frame_update:
+            self.frame.x += self.frame.width
+            if self.frame.x >= self.image.get_width():
+                self.frame.x = 0
+            self.last_frame = current_time
+
+    def draw(self, screen, location, flip=False):
+
+        screen.blit(transform.flip(self.image,True,False) if flip else self.image, location, self.frame) 
