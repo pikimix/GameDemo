4acf5ed59a9f802541fc55bfc1cff324ca5c79fe
Keith Summers <pikimix@gmail.com>
2024-09-29 15:31:20 +0100

    creating debug entities

--- scene.py
+++ scene.py
@@ -2,13 +2,22 @@
 Current state of the game
 """
 import pygame as pg
-from entity import Player
+from entity import Player, Entity
+from random import randint
 
+DEBUG = True
 class Scene:
     def __init__(self) -> None:
-        self._entities = []
         self._server = None
         self._screen = pg.display.set_mode((1280, 720))
+        self._entities = []
+        if DEBUG:
+            # create 5 random entities if we are in debug mode
+            for _ in range(6):
+                loc = pg.Vector2(randint(0,self._screen.get_width()),
+                    randint(0, self._screen.get_height()))
+                self._entities.append(Entity(loc))
+
         self._player = Player(pg.Vector2(self._screen.get_width() / 2, self._screen.get_height() / 2),
                 pg.image.load("assets/player.png").convert_alpha())
 
@@ -21,4 +30,4 @@ class Scene:
         self._screen.fill("purple")
         self._player.draw(self._screen)
         for entity in self._entities:
-            entity.draw()
+            entity.draw(self._screen)
