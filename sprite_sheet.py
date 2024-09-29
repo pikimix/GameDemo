75389a4d21006b2db5907504b48b136cb8c33f34
Keith Summers <pikimix@gmail.com>
2024-09-29 15:55:19 +0100

    prepend _ to self.vars

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -2,19 +2,19 @@ import pygame as pg
 
 class Sprite:
     def __init__(self, image: pg.Surface) -> None:
-        self.image = image
-        self.frame = pg.Rect(0,0,32,64)
-        self.frame_update = 1000/10
-        self.last_frame = 0
+        self._image = image
+        self._frame = pg.Rect(0,0,32,64)
+        self._frame_update = 1000/10
+        self._last_frame = 0
 
     def update(self):
         current_time = pg.time.get_ticks()
-        if current_time > self.last_frame + self.frame_update:
-            self.frame.x += self.frame.width
-            if self.frame.x >= self.image.get_width():
-                self.frame.x = 0
-            self.last_frame = current_time
+        if current_time > self._last_frame + self._frame_update:
+            self._frame.x += self._frame.width
+            if self._frame.x >= self._image.get_width():
+                self._frame.x = 0
+            self._last_frame = current_time
 
     def draw(self, screen: pg.Surface, location: pg.Vector2, color=(0,0,128,255), flip: bool=False):
-        self.image.fill(color,self.frame,pg.BLEND_RGBA_MIN)
-        screen.blit(pg.transform.flip(self.image,True,False) if flip else self.image, location, self.frame) 
+        self._image.fill(color,self._frame,pg.BLEND_RGBA_MIN)
+        screen.blit(pg.transform.flip(self._image,True,False) if flip else self._image, location, self._frame) 
