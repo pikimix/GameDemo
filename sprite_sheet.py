bce13dedbc475134664ee97836f7efa7548d4ba2
Keith Summers <pikimix@gmail.com>
2024-09-29 15:54:56 +0100

    change imports and add typedef to functions

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -1,20 +1,20 @@
-from pygame import Rect, time, transform, BLEND_RGBA_MIN
+import pygame as pg
 
 class Sprite:
-    def __init__(self, image) -> None:
+    def __init__(self, image: pg.Surface) -> None:
         self.image = image
-        self.frame = Rect(0,0,32,64)
+        self.frame = pg.Rect(0,0,32,64)
         self.frame_update = 1000/10
         self.last_frame = 0
 
     def update(self):
-        current_time = time.get_ticks()
+        current_time = pg.time.get_ticks()
         if current_time > self.last_frame + self.frame_update:
             self.frame.x += self.frame.width
             if self.frame.x >= self.image.get_width():
                 self.frame.x = 0
             self.last_frame = current_time
 
-    def draw(self, screen, location, color=(0,0,128,255), flip=False):
-        self.image.fill(color,self.frame,BLEND_RGBA_MIN)
-        screen.blit(transform.flip(self.image,True,False) if flip else self.image, location, self.frame) 
+    def draw(self, screen: pg.Surface, location: pg.Vector2, color=(0,0,128,255), flip: bool=False):
+        self.image.fill(color,self.frame,pg.BLEND_RGBA_MIN)
+        screen.blit(pg.transform.flip(self.image,True,False) if flip else self.image, location, self.frame) 
