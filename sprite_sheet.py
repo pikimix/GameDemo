a89cfb5532079475444f42292d5f0ae34cdec56a
Keith Summers <pikimix@gmail.com>
2024-10-15 22:12:21 +0100

    update sprite to have circle when sheet is None and to generate a mask for collision

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -15,14 +15,21 @@ class SpriteSet:
 
 class Sprite:
     def __init__(self, image: pg.Surface) -> None:
-        self._image = image
-        self._frame = pg.Rect(0,0,32,64)
-        self.width = self._frame.width
-        self.height = self._frame.height
+        self._image = None
+        self._frame = None
+        self.width = 20
+        self.height = 20
+        if image:
+            self._image = image
+            self._frame = pg.Rect(0,0,32,64)
+            self.width = self._frame.width
+            self.height = self._frame.height
+        else:
+            self._image = pg.Surface((self.width,self.height), pg.SRCALPHA)
+            pg.draw.circle(self._image, (255,255,255,255), (10,10), radius=10, width=0)
         self._frame_update = 1000/10
         self._last_frame = 0
 
-    
     def update(self):
         current_time = pg.time.get_ticks()
         if current_time > self._last_frame + self._frame_update:
@@ -31,8 +38,16 @@ class Sprite:
                 self._frame.x = 0
             self._last_frame = current_time
 
+    def get_mask(self, flip) -> pg.Mask:
+        tinted = self._image.copy()
+        surface = pg.Surface((self.width,self.height), pg.SRCALPHA)
+        surface.blit(pg.transform.flip(tinted,True,False) if flip else tinted, (0,0), self._frame) 
+        return pg.mask.from_surface(surface)
+
     def draw(self, screen: pg.Surface, location: pg.Vector2, color:pg.Color|tuple=None, flip: bool=False):
         tinted = self._image.copy()
         if color:
             tinted.fill(color,None,pg.BLEND_RGBA_MIN)
-        screen.blit(pg.transform.flip(tinted,True,False) if flip else tinted, location, self._frame) 
+        screen.blit(pg.transform.flip(tinted,True,False) if flip else tinted, location, self._frame)
+        # mask = self.get_mask(True)
+        # screen.blit(mask.to_surface(),location)
