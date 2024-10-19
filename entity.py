9b2629bc183194daf42621fa49c802a4e0519bee
Keith Summers <pikimix@gmail.com>
2024-10-19 20:27:00 +0100

    Add type hints/ returns, update collision checking to return offset values

--- entity.py
+++ entity.py
@@ -1,3 +1,4 @@
+from __future__ import annotations # To make type hinting work when using classes within this file
 from sprite_sheet import AnimatedSprite, SpriteSet
 import pygame as pg
 import logging
@@ -6,7 +7,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 class Entity:
-    def __init__(self, location: pg.Vector2, sprite: dict=None, uuid=None, name:str=None) -> None:
+    def __init__(self, location: pg.Vector2, sprite: dict=None, uuid:uuid.UUID=None, name:str=None) -> None:
         self.uuid = uuid
         self._location = location
         self._sprite = None
@@ -26,7 +27,7 @@ class Entity:
         self._font = pg.font.SysFont('Futura', 30)
 
     @staticmethod
-    def from_dict(entity: dict, sprite_list: SpriteSet, e_uuid):
+    def from_dict(entity: dict, sprite_list: SpriteSet, e_uuid: uuid.UUID) -> Entity:
         loc = pg.Vector2(entity['location']['x'], entity['location']['y'])
         sprite = None
         if entity['sprite']:
@@ -39,7 +40,7 @@ class Entity:
         new_entity._velocity = pg.Vector2(entity['velocity']['x'], entity['velocity']['y'])
         return new_entity
 
-    def respawn(self, location: pg.Vector2, sprite: dict=None, uuid=None):
+    def respawn(self, location: pg.Vector2, sprite: dict=None, uuid=None) -> None:
         self.uuid = uuid
         self._location = location
         self._sprite = None
@@ -56,12 +57,12 @@ class Entity:
         self._atack = 10
         self.is_alive = True
 
-    def damage(self, damage):
+    def damage(self, damage: int) -> None:
         self._hp -= damage
         if self._hp <= 0:
             self.is_alive = False
 
-    def move_to(self, destination: pg.Vector2):
+    def move_to(self, destination: pg.Vector2) -> None:
         if destination.x > self._location.x:
             self._velocity.x = 200
         elif destination.x < self._location.x:
@@ -76,14 +77,22 @@ class Entity:
         else:
             self._velocity.y = 0
 
-    def check_collides(self, other_entity):
+    def check_collides(self, other_entity:Entity) -> tuple|None:
         mask = self.get_mask()
         other_mask = other_entity.get_mask()
         x_offset = self.get_location().x - other_entity.get_location().x
         y_offset = self.get_location().y - other_entity.get_location().y
-        return mask.overlap(other_mask,(x_offset,y_offset))
+        offeset = None
+        if mask.overlap(other_mask,(x_offset,y_offset)):
+            offeset = (other_entity.get_rect().centerx - self.get_rect().centerx,
+                    other_entity.get_rect().centery - self.get_rect().centery
+            )
+        return offeset
 
-    def get_mask(self):
+    def get_rect(self) -> pg.Rect:
+        return pg.Rect(self._location.x, self._location.y, self._sprite.rect.width, self._sprite.rect.height)
+
+    def get_mask(self) -> pg.Mask:
         if self._sprite:
             return self._sprite.get_mask(flip=self._facing_left)
         else:
@@ -91,7 +100,7 @@ class Entity:
             pg.draw.circle(surface, (255,0,0,255), (10,10), radius=10, width=0)
             return pg.mask.from_surface(surface)
 
-    def get_location(self):
+    def get_location(self) -> pg.Vector2:
         if self._sprite:
             x = self._location.x + (self._sprite.rect.width / 2)
             y = self._location.y + (self._sprite.rect.height / 2)
@@ -99,7 +108,7 @@ class Entity:
         else:
             return self._location
 
-    def update(self, dt: float, bounds:pg.Rect=None):
+    def update(self, dt: float, bounds:pg.Rect=None) -> None:
         self._location += self._velocity * dt
         if bounds:
             if self._location.x < bounds.left:
@@ -111,14 +120,14 @@ class Entity:
             elif self._location.y + self._sprite.rect.height > bounds.bottom:
                 self._location.y = bounds.bottom - self._sprite.rect.height
 
-    def net_update(self, remote_entity:dict):
+    def net_update(self, remote_entity:dict) -> None:
         self._location.x = remote_entity['location']['x']
         self._location.y = remote_entity['location']['y']
         self._velocity.x = remote_entity['velocity']['x']
         self._velocity.y = remote_entity['velocity']['y']
         self._facing_left = remote_entity['facing_left']
         
-    def serialize(self):
+    def serialize(self) -> dict:
         return {
             'uuid': str(self.uuid),
             'location' : { 'x' : self._location.x, 'y': self._location.y},
@@ -128,7 +137,7 @@ class Entity:
             'name' : self._name,
             'is_alive' : self.is_alive
         }
-    def draw(self, screen, color=(255,0,0,255)):
+    def draw(self, screen, color=(255,0,0,255)) -> None:
         if self._sprite:
             self._sprite.draw(screen, self._location, flip=self._facing_left, color=color)
             if self._name:
@@ -146,7 +155,7 @@ class Enemy(Entity):
         super().__init__(location, sprite, uuid, name)
     
     @staticmethod
-    def from_dict(enemy: dict, sprite_list: SpriteSet, e_uuid, target_uuid):
+    def from_dict(enemy: dict, sprite_list: SpriteSet, e_uuid, target_uuid) -> Enemy:
         loc = pg.Vector2(enemy['location']['x'], enemy['location']['y'])
         sprite = None
         if enemy['sprite']:
@@ -156,25 +165,25 @@ class Enemy(Entity):
         new_enemy._velocity = pg.Vector2(enemy['velocity']['x'], enemy['velocity']['y'])
         return new_enemy
     
-    def update(self, dt: float):
+    def update(self, dt: float) -> None:
         return super().update(dt)
     
-    def serialize(self):
+    def serialize(self) -> dict:
         ret_val = super().serialize()
         ret_val['target'] = str(self._target)
         ret_val['type'] = 'enemy'
         return ret_val
     
-    def net_update(self, remote_entity: dict):
+    def net_update(self, remote_entity: dict) -> None:
         if self._target != uuid.UUID(remote_entity['target']):
             logger.error(f'net_update:{self._target=} != {remote_entity["target"]=}')
         # self._target = remote_entity['target']
         return super().net_update(remote_entity)
 
-    def update_target(self, target_uuid):
+    def update_target(self, target_uuid) -> None:
         self._target = target_uuid
     
-    def move_to_target(self, player_position_list:list):
+    def move_to_target(self, player_position_list:list) -> None:
         for player in player_position_list:
             if player['uuid'] == self._target:
                 super().move_to(player['position'])
@@ -204,5 +213,5 @@ class Player(Entity):
         self._sprite.update()
         super().update(dt, bounds)
     
-    def draw(self, screen):
+    def draw(self, screen) -> None:
         super().draw(screen, color=self._color)
