c0282cfade709696fa536efbdf74920924d2ffda
Keith Summers <pikimix@gmail.com>
2024-10-31 21:44:52 +0000

    comment out server side collisoons

--- server/server_main.py
+++ server/server_main.py
@@ -51,12 +51,12 @@ def update(server, dt:float):
             logger.debug('-----')
 
             # # check distance to other entities
-            for other_uuid in [key for key in server.entities.keys() if server.entities[key]['is_alive'] and not e_uuid]:
-                if other_uuid != e_uuid:
-                    # Calculate distance between entities
-                    dx = server.entities[e_uuid]['location']['x'] - server.entities[other_uuid]['location']['x']
-                    dy = server.entities[e_uuid]['location']['y'] - server.entities[other_uuid]['location']['y']
-                    distance = (dx**2 + dy**2)**0.5  # Euclidean distance
+            # for other_uuid in [key for key in server.entities.keys() if server.entities[key]['is_alive'] and not e_uuid]:
+            #     if other_uuid != e_uuid:
+            #         # Calculate distance between entities
+            #         dx = server.entities[e_uuid]['location']['x'] - server.entities[other_uuid]['location']['x']
+            #         dy = server.entities[e_uuid]['location']['y'] - server.entities[other_uuid]['location']['y']
+            #         distance = (dx**2 + dy**2)**0.5  # Euclidean distance
 
             #         # If they are too close, adjust positions
             #         if distance < collision_radius:
@@ -68,13 +68,13 @@ def update(server, dt:float):
             #                 nx = dx / distance
             #                 ny = dy / distance
                             
-                            # Move this entity away from the other entity
-                            server.entities[e_uuid]['location']['x'] += nx * overlap * 0.5  # Adjust position
-                            server.entities[e_uuid]['location']['y'] += ny * overlap * 0.5
+            #                 # Move this entity away from the other entity
+            #                 server.entities[e_uuid]['location']['x'] += nx * overlap * 0.5  # Adjust position
+            #                 server.entities[e_uuid]['location']['y'] += ny * overlap * 0.5
 
-                            # Move the other entity in the opposite direction
-                            server.entities[other_uuid]['location']['x'] -= nx * overlap * 0.5
-                            server.entities[other_uuid]['location']['y'] -= ny * overlap * 0.5
+            #                 # Move the other entity in the opposite direction
+            #                 server.entities[other_uuid]['location']['x'] -= nx * overlap * 0.5
+            #                 server.entities[other_uuid]['location']['y'] -= ny * overlap * 0.5
 
 if __name__ == '__main__':
     server = WebSocketServer()
